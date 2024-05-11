fda788240af9d588f2657dcf5c0b7938739b78ba
Valentin Catalin Bengea <valentinbengea@gmail.com>
2024-05-11 19:35:16 +0200

    Implement playRound function

--- main.js
+++ main.js
@@ -14,3 +14,53 @@ function getHumanChoice() {
     }
     }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return ("Tie!");
+    }
+
+    else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            computerScore ++;
+            return "You lose! Paper beats Rock";
+        }
+        else {
+            humanScore ++;
+            return "You win! Rock beats Scissors";
+
+        }
+    }
+
+    else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            humanScore ++;
+            return "You win! Paper beats Rock";
+        }
+
+        else {
+            computerScore ++;
+            return "You lose! Scissors beats Paper";
+        }
+    }
+
+    else if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
+            humanScore ++;
+            return "You Win! Scissors beats Paper";
+        }
+
+        else {
+            computerScore++;
+            return "You lose! Rock beats Scissors";
+        }
+    }
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
